8a5fe82f280c677e220a07f6ccfe1df9f928a3e4
Shamal Williams <shamal@Shamals-MacBook-Pro.local>
2021-04-16 17:34:07 -0400

    FinishedFinally

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Character from './components/Character';
+import axios from 'axios';
 import './App.css';
 
-const App = () => {
+const App = () => { 
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [data, setData] = useState([])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios
+    .get('https://swapi.py4e.com/api/people/')
+    .then(response => {
+      console.log(response)
+      setData(response.data.results)
+    })
+    .catch(err => console.log(err))
+  },[])
+
+  console.log(data)
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+        {data.map((char, index)=>
+          <Character key={char.url} data={data[index]}/>
+        )}
     </div>
   );
 }
-
+  
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,20 @@
 // Write your Character component here
+import React from 'react';
+//import styled from 'styled-components';
+
+function Character(props){
+    console.log(props)
+
+    return(
+        <div className='Character'> 
+            <p>Name: {props.data.name}</p>
+            <p>Height: {props.data.height}</p>
+            <p>Eye Color: {props.data.eye_color}</p>
+            <p>Hair Color: {props.data.hair_color}</p>
+            <p>Gender: {props.data.gender}</p>
+            <p>Skin Color: {props.data.skin_color}</p>
+        </div> 
+    )
+}
+
+export default Character 
